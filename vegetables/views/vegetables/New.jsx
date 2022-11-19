ef0d0f9f2b1f4b6494ae4952566cca83a5cb63b6
AbelMadera <110073639+AbelMadera@users.noreply.github.com>
2022-11-19 10:20:04 -0600

    Add files via upload

---
+++ vegetables/views/vegetables/New.jsx
@@ -0,0 +1,22 @@
+const React = require('react');
+
+class New extends React.Component {
+    render(){
+        return(
+            <>
+            <h1>Create A New Vegetable</h1>
+            <nav>
+                <a href="/vegetables">Go Back To Vegetables Home Page</a>
+            </nav>
+            <form method="POST" action="/vegetables">
+                Name: <input type="text" name="name" placeholder='Vegetable Name'></input><br/>
+                Color: <input type="text" name="color" placeholder='Vegetable Color'></input><br/>
+                Ready To Eat: <input type="checkbox" name="readyToEat"></input><br/>
+                <input type="submit" value="Submit Vegetable"></input>
+            </form>
+            </>
+        )
+    }
+}
+
+module.exports = New 
